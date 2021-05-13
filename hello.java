48178ec60a71ded26e8bb0a13bd0c629e311c58d
war19047 <war19047@byui.edu>
2021-05-12 23:07:22 -0600

    My first program in java

---
+++ hello.java
@@ -0,0 +1,14 @@
+
+/*
+My first program in Java.
+*/
+   class HelloWorld {
+       /*
+       The program begins with a call to main() this is how 
+       all methods and functions are executed.
+       */
+       public static void main(String args[])
+       {
+           System.out.println("Hello World! This is my first program in Java.");
+       }
+   }
